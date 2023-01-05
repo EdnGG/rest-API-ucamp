9481bab7e6168094ec3469d05efe1289d1211dbb
Eden <gresseden@gmail.com>
2023-01-05 17:06:16 -0500

    editing guitars.routes.js file

--- src/routes/guitars.routes.js
+++ src/routes/guitars.routes.js
@@ -8,12 +8,12 @@ const {
 } = require("../controllers/guitar.controller.js");
 const { tokenVerification } = require("../middlewares/authToken.js");
 
-router.get("/movies", getAllGuitars);
+router.get("/guitars", getAllGuitars);
 
-router.post("/movies", tokenVerification, postGuitar);
+router.post("/guitar", tokenVerification, postGuitar);
 
-router.put("/movies/:id", tokenVerification, updateGuitar);
+router.put("/guitar/:id", tokenVerification, updateGuitar);
 
-router.delete("/movies/:id", tokenVerification, deleteGuitar);
+router.delete("/guitar/:id", tokenVerification, deleteGuitar);
 
 module.exports = router;
